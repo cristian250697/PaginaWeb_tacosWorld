187e2fe5ae35128074cfc0d4cb79e69e3344db5b
cristian15240613 <43045119+cristian15240613@users.noreply.github.com>
2018-11-29 14:22:44 -0600

    Restructuracion

--- PHP/Conexion.php
+++ PHP/Conexion.php
@@ -5,7 +5,7 @@ function conectar(){
 $user="root";
 $pass="";
 $server="localhost";
-$db="TACOSWORL";
+$db="TACOSWORLD";
     
 $conection= mysqli_connect($server,$user,$pass,$db) or die ("Error al conectar a la base de datos".mysqli_error());    
     
