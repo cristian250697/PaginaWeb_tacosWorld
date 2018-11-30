a40eeb67dd22a78b0ca538adb1ef6609a3f7010e
maartinrc <martin_coss.0@hotmail.com>
2018-11-29 20:24:46 -0600

    Se corrigieron errores de ubicacón en el mapa para la versión de móvil

--- js/mapaTaqueria.js
+++ js/mapaTaqueria.js
@@ -1,28 +1,40 @@
  var marcador;
-let mapaTaqueria = L.map('mapaTaqueria').fitWorld();
+let mapaTaqueria ;
+
+mapaTaqueria= L.map('mapaTaqueria').setView([21.128758,-101.681130],12);
 
 
 mapaTaqueria.locate({setView: true, maxZoom: 19});
 //.setView([21.128758,-101.681130],12)
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,  TacosWorld SMC'
-}).addTo(mapaTaqueria);
+}).addTo(mapaTaqueria); 
+
+/*L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,  TacosWorld SMC',
+		id: 'mapbox.streets'
+	}).addTo(mapaTaqueria);*/
 
 
+
+
+mapaTaqueria.on('locationfound',onLocationFound);   
+mapaTaqueria.on('locationerror',onLocationError);
 
 function onLocationFound (e){
     marcador =  L.marker(e.latlng,{draggable: true}).addTo(mapaTaqueria);
     marcador.bindPopup("Ubica tu taquería").openPopup();
 }
-mapaTaqueria.on('locationfound',onLocationFound);   
-
 function onLocationError(e){
-   e.alert("no aceptaste la ubicacion");
-    
+    alert("no aceptaste la ubicacion, para ofrecerte un mejor servicio, por favor, actívala.");
+    marcador = L.marker([21.128758,-101.681130],{draggable: true}).addTo(mapaTaqueria);
+    marcador.bindPopup("Ubica tu taquería").openPopup();
+  
     
 }
-mapaTaqueria.on('locationerror',onLocationError);
 
+// Marca de agua
 L.Control.Watermark = L.Control.extend({
     onAdd: function(mapaTaqueria) {
         var img = L.DomUtil.create('img');
@@ -43,6 +55,7 @@ L.control.watermark = function(opts) {
 }
 
 L.control.watermark({ position: 'bottomleft' }).addTo(mapaTaqueria);
+//Obtener datos
 
 function obtenDatos(){
 
