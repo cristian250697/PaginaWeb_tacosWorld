aa5f724b6e19c9185b67ef800621918f7894c275
maartinrc <martin_coss.0@hotmail.com>
2018-12-01 12:29:10 -0600

    Cambios importantes
    Se establecio el conjunto de caracteres predeterminado a usar cuando se envían datos desde y hacia el servidor de la base de datos.
    Ahora el mapa muestra el nombre de las taquerías que el usuario tiene cerca, en lugar de sólo mostrar el número.

--- PHP/Conexion.php
+++ PHP/Conexion.php
@@ -8,7 +8,7 @@ $server="localhost";
 $db="TACOSWORLD";
     
 $conection= mysqli_connect($server,$user,$pass,$db) or die ("Error al conectar a la base de datos".mysqli_error());    
-    
+mysqli_set_charset($conection,"utf8"); //Establece el conjunto de caracteres predeterminado a usar cuando se envían datos desde y hacia el servidor de la base de datos.
 return $conection;
 }
 ?>
